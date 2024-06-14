db72b913b8192f46ee01aa8da2c041cdb1ecf14f
Diego Nassula <diego.nassula@digihub.com.br>
2024-06-14 14:18:18 -0300

    Commit inicial

---
+++ src/main/java/br/com/diegonassula/msgerenciamentopedidos/MsGerenciamentoPedidosApplication.java
@@ -0,0 +1,15 @@
+package br.com.diegonassula.msgerenciamentopedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGerenciamentoPedidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsGerenciamentoPedidosApplication.class, args);
+    }
+
+
+
+}
---
+++ src/test/java/br/com/diegonassula/msgerenciamentopedidos/MsGerenciamentoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diegonassula.msgerenciamentopedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGerenciamentoPedidosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
